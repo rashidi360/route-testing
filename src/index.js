9165606e8e678978e62a86f16922f9bf1d3474c8
rashidi360 <rashids@i360.lk>
2023-08-04 10:29:48 +0530

    new version type2 updated

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import "assets/css/App.css";
-import AdminLayout from "layouts/admin";
-import AuthLayout from "layouts/auth";
-import RtlLayout from "layouts/rtl";
 import React from "react";
 import ReactDOM from "react-dom";
 import {
@@ -11,11 +8,28 @@ import {
   Route,
   Routes,
   createRoutesFromElements,
+  createBrowserRouter,
+  RouterProvider,
 } from "react-router-dom";
 import theme from "theme/theme";
+// Layoutes
+import AdminLayout from "layouts/admin";
+import AuthLayout from "layouts/auth";
+import RtlLayout from "layouts/rtl";
 import RootLayout from "layouts/RootLayout";
+//Pages
+// Admin Imports
+import MainDashboard from "views/admin/default";
+import NFTMarketplace from "views/admin/marketplace";
+import Profile from "views/admin/profile";
+import DataTables from "views/admin/dataTables";
+import RTL from "views/admin/rtl";
 
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+// Auth Imports
+import SignInCentered from "views/auth/signIn";
+
+//--------------------New Version Type1----------------------
+
 
 const router = createBrowserRouter([
   {
@@ -41,24 +55,38 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <RouterProvider router={router} />
 );
 
+//--------------------New Version Type2----------------------
+//--------------------No Need Route.js-----------------------
+ 
 // const router = createBrowserRouter(
 //   createRoutesFromElements(
-//     <Route path="/" element={<RootLayout />}>
-//       <Route path={`auth`} element={<AuthLayout />} />
-//       <Route path={`admin`} element={<AdminLayout />} />
-//       <Route path={`rtl`} element={<RtlLayout />} />
-//       <Route index path="/" element={<Navigate to="admin" />} />
+//     <Route path="/" element={<Navigate to="admin" />}>
+//       <Route path={`auth`} element={<AuthLayout />} >
+//         <Route path="sign-in" element={<SignInCentered />} />
+//       </Route>
+//       <Route path={`admin`} element={<AdminLayout />}>
+//         <Route path="default" element={<MainDashboard />} />
+//         <Route path="nft-marketplace" element={<NFTMarketplace />} />
+//         <Route path="data-tables" element={<DataTables />} />
+//         <Route path="profile" element={<Profile />} />
+//       </Route>
+//       <Route path={`rtl`} element={<RtlLayout />} >
+//         <Route path="rtl-default" element={<RTL />} />
+//       </Route>
+//       {/* <Route index path="/" element={<Navigate to="admin" />} /> */}
 //     </Route>
 //   )
 // );
 
-// function index(){
+// function routes(){
 //   return(
 //     <RouterProvider router={router} />
 //   )
 // }
+// export default routes
 
-// export default index
+
+//----------------Old Version-------------------
 
 // ReactDOM.render(
 //   <ChakraProvider theme={theme}>
